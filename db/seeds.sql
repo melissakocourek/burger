5c6742a24cd363962da356b7326b9f601049a254
Melissa Kocourek <melissakocourek@gmail.com>
2019-05-02 23:30:35 -0500

    initial site files

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+ 
+INSERT INTO burgers (burger_name, devoured)
+ VALUES('Beef Double Cheese Burger', false),
+('Patty Melt Burger',false),
+('Mushroom & Swiss Double Cheese Burger',false);
+
+select * from burgers;
+
+
